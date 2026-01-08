b7d3fbc7766f6ec6e6506aeadb22946670f6980f
Herman <97112+frek818@users.noreply.github.com>
2026-01-07 20:47:46 -0800

    teaching day 1 AOC

---
+++ aoc-day1/main.py
@@ -0,0 +1,60 @@
+class Code():
+    def __init__(self, direction: str, distance: int):
+        self.direction = direction
+        self.distance = distance
+
+
+class Lock():
+    def __init__(self, start: int, start_range: int = 0, end_range: int = 99):
+        self.position = start
+        self.start_range = start_range
+        self.end_range = end_range
+
+    def _click(self, direction):
+        # Move the dial on point to left of the right
+        pass
+
+    def enter_code(self, code: Code):
+        # Enter the code into the lock using the _click method
+        pass
+
+    def get_current_value(self) -> int:
+        # gets the current value of the lock
+        return self.position
+
+    
+
+
+def read_combos(filename: str) -> list:
+    # read the file and parse into lines into combo
+    
+    combos = list()
+    # Make it do it thing
+    return combos
+
+def main():
+    # 0. Create zero tracker variable
+    landed_on_zero = 0
+
+    # 1. read the input combination
+    combos = read_combos("problems.txt")
+
+    # 2. Get the lock
+    lock = Lock(start=50)
+
+    # 3. Enter each combination 1-by-1
+    for code in combos:
+        lock.enter_code(code)
+        current_value = lock.get_current_value()
+
+        # 4. Keep track of when the lock lands on 0
+        if current_value == 0:
+            landed_on_zero = landed_on_zero + 1
+
+    # 5. print out the answer
+    print("The answers is: ", landed_on_zero)
+
+
+
+if __name__ == "__main__":
+    main()
